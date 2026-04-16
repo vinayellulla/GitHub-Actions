eb1e5c28fb0cf8da2eb28311ffdbd7a6f42d9957
vinayellulla <135804025+vinayellulla@users.noreply.github.com>
2026-04-16 13:01:05 +0530

    Add region to S3 backend configuration

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "qrcodestore-123"
     key            = "remotebackend/terraform.tfstate"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraform-lock"
   }
 }

5b07b9e6f5ce2360dd3c39c465d5570fbb814650
vinayellulla <135804025+vinayellulla@users.noreply.github.com>
2026-04-16 11:59:44 +0530

    Add AWS provider and S3 bucket resource

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = ap-southeast-2
+}
+
+resource "random_string" "example" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "vinayellulla-${random_string.example.result}"
+
+  tags = {
+    Name        = "My Bucket"
+    Environment = "GitHub Runners"
+  }
+
+}

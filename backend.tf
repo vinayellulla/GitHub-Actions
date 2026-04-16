fd1cb2dde7a96ecdd310f98bc216701a07f956f6
vinayellulla <135804025+vinayellulla@users.noreply.github.com>
2026-04-16 12:59:35 +0530

    Remove region from S3 backend configuration
    Removed region configuration from S3 backend.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "qrcodestore-123"
     key            = "remotebackend/terraform.tfstate"
-    region         = "ap-southeast-2"
     dynamodb_table = "terraform-lock"
   }
 }
